d4b18081b4565a79a9c3bd309e8763f9853e6b83
marnixah <marnix@hagehoeve.com>
2021-12-02 13:35:35 +0100

    feat: handler field

--- classes/live_job.py
+++ classes/live_job.py
@@ -10,10 +10,12 @@ class live_job(db.Model, SerializerMixin):
     status = db.Column(db.String, index=True, nullable=False)
     save_location = db.Column(db.String, nullable=False)
     url = db.Column(db.String, unique=True, nullable=False)
+    handler = db.Column(db.String, nullable=False)
     error = db.Column(db.String, nullable=True)
 
-    def __init__(self, url, save_location, status="pending"):
+    def __init__(self, url, save_location, handler="yt-dlp", status="pending"):
         self.url = url
         self.save_location = save_location
         self.status = status
+        self.handler = handler
         self.error = None
