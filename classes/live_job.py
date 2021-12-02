7ea36180cc5f98e5b3ce4c64c75d7dc8c7bca68b
marnixah <marnix@hagehoeve.com>
2021-12-02 11:57:47 +0100

    feat: different way of splitting sqlalchemy

--- classes/live_job.py
+++ classes/live_job.py
@@ -1,26 +1,19 @@
-from flask import current_app as app
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.inspection import inspect
 from sqlalchemy_serializer import SerializerMixin
-from os import environ
+from flask_sqlalchemy import SQLAlchemy
 
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
 class live_job(db.Model, SerializerMixin):
     __tablename__ = 'live_jobs'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     status = db.Column(db.String, index=True, nullable=False)
+    save_location = db.Column(db.String, nullable=False)
     url = db.Column(db.String, unique=True, nullable=False)
+    error = db.Column(db.String, nullable=True)
 
     def __init__(self, url, save_location, status="pending"):
         self.url = url
         self.save_location = save_location
         self.status = status
-
-# Create tables if they don't exist
-if not inspect(db.engine).has_table('live_jobs'):
-    print("Creating tables")
-    db.create_all()
+        self.error = None
