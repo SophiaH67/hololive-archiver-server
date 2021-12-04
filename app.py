2d518d2f7e953c81779eaf6a420fefbf283b8918
marnixah <marnix@hagehoeve.com>
2021-12-04 14:50:30 +0100

    fix: cors

--- app.py
+++ app.py
@@ -32,5 +32,14 @@ async def startup():
     app.add_background_task(update_jobs)
 
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,PATCH')
+    return response
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
